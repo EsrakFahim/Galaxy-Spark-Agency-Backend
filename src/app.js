6d42c2d79c374c5c79862506e799919adab48b93
EsrakFahim <efahim0656@gmail.com>
2024-11-01 03:53:08 +0600

    refactor: Update CORS configuration and middleware setup
    Refactor the CORS configuration and middleware setup in the app.js file.
    Instead of allowing all origins, the CORS configuration now allows only the specified frontend origin.
    The middleware setup has been modified to use the specified CORS options.
    This change improves security and restricts access to the API from unauthorized origins.

--- src/app.js
+++ src/app.js
@@ -4,26 +4,23 @@ import cors from "cors";
 
 const app = express();
 
+// CORS configuration
+const corsOptions = {
+      origin: "http://localhost:3000", // Allow your frontend origin
+      credentials: true, // Allow cookies to be sent
+};
+
 // Middleware setup
-app.use(
-      cors({
-            origin: "*", // CORS configuration for the specified origin
-            credentials: true, // Allow cookies
-      })
-);
-app.use(cookieParser()); // Enable cookie parsing
-app.use(express.json({ limit: "50mb" })); // Enable JSON parsing with a size limit
-app.use(express.urlencoded({ extended: true, limit: "50mb" })); // Support for URL-encoded data
+app.use(cors(corsOptions)); // Use the specified CORS options
+app.use(cookieParser());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.static("public")); // Serve static files from 'public' directory
 
 // Routes import
 import clientRouter from "./routes/client.route.js";
 
 // Routes setup
-
-
-// # client routes
 app.use("/api/v1/client", clientRouter); // Mount client routes
-
 
 export { app };
