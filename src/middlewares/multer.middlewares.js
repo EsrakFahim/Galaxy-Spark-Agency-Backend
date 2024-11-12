ac403aab4fd7376d73199bffad0e798df6657d0b
EsrakFahim <efahim0656@gmail.com>
2024-11-12 14:29:25 +0600

    refactor: Change multer temp directory to use OS temporary directory and ensure its existence

--- src/middlewares/multer.middlewares.js
+++ src/middlewares/multer.middlewares.js
@@ -1,17 +1,17 @@
 import multer from "multer";
-import fs from "fs";
+import os from "os";
 import path from "path";
+import fs from "fs";
 
-const tempDir = path.join(process.cwd(), "public/temp");
-
-// Ensure the 'public/temp' directory exists at runtime
+// Ensure that the directory exists
+const tempDir = path.join(os.tmpdir(), "uploads");
 if (!fs.existsSync(tempDir)) {
       fs.mkdirSync(tempDir, { recursive: true });
 }
 
 const storage = multer.diskStorage({
       destination: function (req, file, cb) {
-            cb(null, tempDir); // Use the ensured directory
+            cb(null, tempDir); // Save files in /tmp/uploads
       },
       filename: function (req, file, cb) {
             const uniqueSuffix =
